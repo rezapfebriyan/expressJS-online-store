c16650a66e2bc4aa7419d566863bbb71f9f7777b
rezapfebriyan <rezafebriyan00@gmail.com>
2023-04-04 04:22:17 +0700

    add migration and seeder users table

---
+++ migrations/20230330004427_users_migration.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('users', function (table) {
+            table.increments('id');
+            table.string('name', 30).notNullable();
+            table.string('email', 30).notNullable();
+            table.string('password', 30).notNullable();
+            table.string('phone', 14).notNullable();
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTable('users');
+};
---
+++ seeds/user_seeder.js
@@ -0,0 +1,9 @@
+exports.seed = async (knex) => {
+  // Deletes ALL existing entries
+  return knex('users').del()
+   .then( () => {
+     return knex('users').insert([
+       {id: 1, name: 'Reza Putra Febriyan', email: 'rezafebriyan00@gmail.com', password: 'persija1928', phone: 6287837255556}
+      ]);
+    })
+}
