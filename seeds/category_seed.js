15372b6d5defa4d9be999408ac5ade3e8ef6a8ea
rezapfebriyan <rezafebriyan00@gmail.com>
2023-04-06 05:54:03 +0700

    add category seeder

---
+++ seeds/category_seed.js
@@ -0,0 +1,13 @@
+exports.seed = async (knex) => {
+  // Deletes ALL existing entries
+  return knex('categories').del()
+   .then( () => {
+     return knex('categories').insert([
+      {id: 1, name: 'Learn Go Gin'},
+      {id: 2, name: 'Learn Spring Boot'},
+      {id: 3, name: 'Learn Nest JS'},
+      {id: 4, name: 'Learn Ruby on Rails'},
+      {id: 5, name: 'Learn Python Django'}
+      ]);
+    })
+}
