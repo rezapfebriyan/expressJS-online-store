084504292a31cc8b12234b5abd7ee7df1da54d62
rezapfebriyan <rezafebriyan00@gmail.com>
2023-04-06 07:17:34 +0700

    add migration coupons table

---
+++ migrations/20230406000814_coupon_migration.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('coupons', function (table) {
+            table.increments('id');
+            table.string('code', 20).notNullable();
+            table.string('description', 50).notNullable();
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTable('coupons');
+};
