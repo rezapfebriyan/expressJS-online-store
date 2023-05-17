636f542ed701cbda9b77dab9cd1db7e565eb8d76
rezapfebriyan <rezafebriyan00@gmail.com>
2023-05-18 06:09:24 +0700

    refactoring category module

--- modules/category/repository/index.js
+++ modules/category/repository/index.js
@@ -1,26 +1,21 @@
 const table = 'categories'
 
 module.exports = (knex) => {
-    module.getAllCategories = () => {
-        return knex.select().table(table)
-    }
-    module.getCategoryById = (id) => {
-        return knex.select()
-            .where("id", id).first()
-            .table(table)
-    }
-    module.createCategory = (body) => {
-        return knex.table(table).insert(body)
-    }
-    module.updateCategory = (id, body) => {
-        return knex.table(table)
-            .where("id", id)
-            .update(body)
-    }
-    module.deleteCategory = (id, body) => {
-        return knex.table(table)
-            .where("id", id)
-            .del()
-    }
+    module.getAllCategories = () => knex.select().table(table)
+
+    module.getCategoryById = (id) => knex.select()
+        .where("id", id).first()
+        .table(table)
+
+    module.createCategory = (body) => knex.table(table).insert(body)
+
+    module.updateCategory = (id, body) => knex.table(table)
+        .where("id", id)
+        .update(body)
+
+    module.deleteCategory = (id) => knex.table(table)
+        .where("id", id)
+        .del()
+        
     return module
 }
--- modules/category/usecase/index.js
+++ modules/category/usecase/index.js
@@ -1,18 +1,13 @@
 module.exports = (repository) => {
-    module.getAllCategories = () => {
-        return repository.getAllCategories()
-    }
-    module.getCategoryById = (id) => {
-        return repository.getCategoryById(id)
-    }
-    module.createCategory = (body) => {
-        return repository.createCategory(body)
-    }
-    module.updateCategory = (id,body) => {
-        return repository.updateCategory(id, body)
-    }
-    module.deleteCategory = (id) => {
-        return repository.deleteCategory(id)
-    }
+    module.getAllCategories = () => repository.getAllCategories()
+
+    module.getCategoryById = (id) => repository.getCategoryById(id)
+
+    module.createCategory = (body) => repository.createCategory(body)
+
+    module.updateCategory = (id,body) => repository.updateCategory(id, body)
+
+    module.deleteCategory = (id) => repository.deleteCategory(id)
+    
     return module
 }
