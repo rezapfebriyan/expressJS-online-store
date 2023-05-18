37b6a386ff0a94361dfed4aee65f30d841dd814d
rezapfebriyan <rezafebriyan00@gmail.com>
2023-05-18 08:20:24 +0700

    entry point for API coupon

--- app.js
+++ app.js
@@ -20,70 +20,9 @@ const categoriesRepository = repository.newCategoriesRepository(knex)
 const categoriesUseCase = usecase.newCategoriesUseCase(categoriesRepository)
 delivery.newCategoriesController(app, categoriesUseCase)
 
-app.get('/coupons', async (req, res ) => {
-    try {
-        let coupons = await knex('coupons');
-        res.json({
-            'status': 200,
-            data: coupons
-        })
-    } catch (err) {
-        res.json({
-            'status': 500,
-            message: err
-        })
-    }
-})
-
-app.get('/coupon/:id', async (req, res) => {
-    try {
-        let coupons = await knex('coupons').where('id', req.params.id);
-        res.json({
-            'status': 200,
-            data: coupons
-        })
-    } catch (err) {
-        res.json({
-            'status': 500,
-            message: err
-        })
-    }
-})
-
-app.post('/coupon', async (req, res) => {
-    try {
-        let code = req.body.code        
-        let description = req.body.description        
-        await knex('coupons').insert({ code, description })
-
-        res.json({ 
-            'status': 201,
-            message: 'Coupon has been created'
-        })
-    } catch (err) {
-        res.json({
-            'status': 500,
-            message: err
-        })
-    }
-})
-
-app.put('/coupon/:id', async (req, res) => {
-    try {
-        let code = req.body.code
-        let description = req.body.description
-        await knex('coupons').where('id', req.params.id).update({ code, description })        
-        res.json({ 
-            'status': 200,
-            message: 'Coupon has been updated'
-        })
-    } catch (err) {
-        res.json({
-            'status': 500,
-            message: err
-        })
-    }
-})
+const couponsRepository = repository.newCouponsRepository(knex)
+const couponsUseCase = usecase.newCouponsUseCase(couponsRepository)
+delivery.newCouponsController(app, couponsUseCase)
 
 app.listen(port, () => {
     console.log(`Server listen and running in port ${port}`)
