c0440cbb761395d997f6ec1c9cff90dcbae8654c
rezapfebriyan <rezafebriyan00@gmail.com>
2023-05-21 09:34:10 +0700

    update utils

--- utils.js
+++ utils.js
@@ -2,23 +2,15 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const secret = "$2b%10@IptGst5LQEfTvYNGoKoNOLK"
 
-const hashPassword = password => {
-  return bcrypt.hashSync(password, secret)
-}
+const hashPassword = password => bcrypt.hashSync(password, secret)
 
-const comparePassword = (password, hash) => {
-  return bcrypt.compareSync(password, hash)
-}
+const comparePassword = (password, hash) => bcrypt.compareSync(password, hash)
 
-const generateJwt = payload => {
-    return jwt.sign(payload, secret, {
-        expiresIn: '1h'
-    })
-}
+const generateJwt = payload => jwt.sign(payload, secret, {
+  expiresIn: '1h'
+})
   
-const verifyJwt = token => {
-    return jwt.verify(token, secret)
-}
+const verifyJwt = token => jwt.verify(token, secret)
 
 module.exports={
     hashPassword, comparePassword, generateJwt, verifyJwt
