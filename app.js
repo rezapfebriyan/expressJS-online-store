f3bf8f2b90fb13b42e3f7ad0af52b6e42faa8a6f
rezapfebriyan <rezafebriyan00@gmail.com>
2023-05-26 07:50:42 +0700

    fix: entrypoint API product

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const dotenv = require('dotenv').config()
+require('dotenv').config()
+const knex = require('./knex')
 
 const auth = require('./middleware/auth')
-const knex = require('./knex')
 const { comparePassword,
     generateJwt,
     hashPassword }
@@ -16,13 +16,21 @@ const port = process.env.APP_PORT
 
 app.use(express.json())
 
-const categoriesRepository = repository.newCategoriesRepository(knex)
-const categoriesUseCase = usecase.newCategoriesUseCase(categoriesRepository)
+const categoriesRepository = repository.newCategoriesRepository(knex) // include param knex ORM
+const categoriesUseCase = usecase.newCategoriesUseCase(categoriesRepository) // include param repository
 delivery.newCategoriesController(app, categoriesUseCase)
 
 const couponsRepository = repository.newCouponsRepository(knex)
 const couponsUseCase = usecase.newCouponsUseCase(couponsRepository)
 delivery.newCouponsController(app, couponsUseCase)
+
+const productsRepository = repository.newProductsRepository(knex)
+const productsUseCase = usecase.newProductsUseCase(productsRepository)
+delivery.newProductsController(app, productsUseCase)
+
+const usersRepository = repository.newUsersRepository(knex)
+const usersUseCase = usecase.newUsersUseCase(usersRepository)
+delivery.newUsersController(app, usersUseCase)
 
 app.listen(port, () => {
     console.log(`Server listen and running in port ${port}`)
