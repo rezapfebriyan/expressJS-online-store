f87245521de200dc078b3ddaad044cab7e15891e
rezapfebriyan <rezafebriyan00@gmail.com>
2023-08-14 10:36:23 +0700

    update data seeder

--- seeds/coupon_seeder.js
+++ seeds/coupon_seeder.js
@@ -1,12 +1,10 @@
 exports.seed = async (knex) => {
   // Deletes ALL existing entries
   return knex('coupons').del()
-   .then( () => {
-     return knex('coupons').insert([
-      {id: 1, code: 'AKHIRTAHUN', description: "This is coupon for shop on last year"},
-      {id: 2, code: 'HASBOLNAS', description: "This is coupon for shop on shooping online day"},
-      {id: 3, code: 'GAJIAN', description: "This is coupon for shop when employee's payroll landed"},
-      {id: 4, code: 'THR', description: 'This is coupon for shop on last day ramadhan'}
-      ]);
-    })
+   .then( () => knex('coupons').insert([
+       { id: 1, code: 'AKHIRTAHUN', description: "This is coupon for shop on last year" },
+       { id: 2, code: 'HASBOLNAS', description: "This is coupon for shop on shooping online day" },
+       { id: 3, code: 'GAJIAN', description: "This is coupon for shop when employee's payroll landed" },
+       { id: 4, code: 'THR', description: 'This is coupon for shop on last day ramadhan' }
+   ]))
 }
